934d9cc5b76dc69293a8212f42a71d95c33c1100
emilekamana <emileizere@gmail.com>
2025-01-05 16:49:46 +0200

    chore: connecting to MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import * as mongoDB from "mongodb";
 
 //For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
+
+export async function connectToDatabase () {
+  dotenv.config();
+
+  const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING as string);
+          
+  await client.connect();
+      
+  const db: mongoDB.Db = client.db(process.env.DB_NAME);
+
+  console.log(`Successfully connected to database: ${db.databaseName}`);
+}
+
+try {
+  connectToDatabase(); 
+} catch (error) {
+  console.log('error during connecting to mongo: ');
+  console.error(error);
+}
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express & TypeScript Server");
